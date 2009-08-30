e3194b46282529e9a7758b6fa9676094ece2e909
richard_szalay_cp <richard@richardszalay.com>
2009-08-30 16:20:40 +0000

    Moved all files into /trunk

---
+++ applications/Setup/build/build.bat
@@ -0,0 +1,1 @@
+..\tools\wix\candle.exe ..\source\RichardSzalay.HostsFileExtension.Setup\RichardSzalay.HostsFileExtension.Setup.wix
---
+++ applications/Setup/source/RichardSzalay.HostsFileExtension.Setup.Actions/CustomAction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace RichardSzalay.HostsFileExtension.Setup.Actions
+{
+    public class CustomActions
+    {
+        private const string UIModuleKey = "ManageHosts";
+        private const string ExtensionQualifiedName = "RichardSzalay.HostsFileExtension.Registration.ManageHostsModuleUIProvider, RichardSzalay.HostsFileExtension, Version=1.0.0.0, Culture=neutral, PublicKeyToken=20cb46ecc35c2f94, processorArchitecture=MSIL";
+
+        [CustomAction]
+        public static ActionResult InstallUIModule(Session session)
+        {
+            session.Log("Installing UIModuleProvider {0} as {1}", ExtensionQualifiedName, UIModuleKey);
+
+            IisInstallUtil.AddUIModuleProvider(UIModuleKey, ExtensionQualifiedName);
+
+            session.Log("Installation of UIModuleProvider complete");
+
+            return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult UninstallUIModule(Session session)
+        {
+            try
+            {
+                session.Log("Uninstalling UIModuleProvider {0}", UIModuleKey);
+
+                IisInstallUtil.RemoveUIModuleProvider(UIModuleKey);
+
+                session.Log("Uninstallation of UIModuleProvider complete");
+            }
+            catch (Exception ex)
+            {
+                session.Log("UI Module removal failed: {0}", ex.Message);
+
+                return ActionResult.Failure;
+            }
+
+            return ActionResult.Success;
+        }
+    }
+}
