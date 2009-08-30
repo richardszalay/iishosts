21e00184b333bc2f942dfcda50e822ffb52263de
richard_szalay_cp <SND\richard_szalay_cp>
2009-08-30 16:20:40 +0000

    Moved all files into /trunk

---
+++ applications/Setup/IisExtension/source/RichardSzalay.HostsFileExtension/Presenter/ManageHostsModulePagePresenter.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RichardSzalay.HostsFileExtension.View;
+using System.Drawing;
+using Microsoft.Web.Management.Client;
+using Microsoft.Web.Management.Client.Win32;
+using RichardSzalay.HostsFileExtension.Properties;
+using System.Collections;
+using System.Resources;
+
+namespace RichardSzalay.HostsFileExtension.Presenter
+{
+    public class ManageHostsModulePagePresenter
+    {
+        private IManageHostsModulePage view;
+        private HostsFile hostsFile;
+
+        private string filter;
+
+        public ManageHostsModulePagePresenter(IManageHostsModulePage view)
+        {
+            this.view = view;
+            this.view.Initialized += new EventHandler(view_HandleCreated);
+            this.view.Refreshing += new EventHandler(view_Refreshing);
+        }
+
+        void view_Refreshing(object sender, EventArgs e)
+        {
+            this.UpdateData();
+        }
+
+        void view_HandleCreated(object sender, EventArgs e)
+        {
+            view.SetTaskList(new ManageHostsTaskList(this));
+
+            this.UpdateData();
+        }
+
+        public bool HasChanges
+        {
+            get
+            {
+                return hostsFile.IsDirty;
+            }
+        }
+
+        private void UpdateData()
+        {
+            hostsFile = new HostsFile();
+
+            DisplayEntries();
+        }
+
+        private IEnumerable<HostEntry> SelectedEntries
+        {
+            get { return view.SelectedEntries; }
+        }
+
+        private bool CanApplyChanges
+        {
+            get { return true; }
+        }
+
+        private void AddTask()
+        {
+            using (var form = new EditHostEntryForm(view.ServiceProvider, new DnsAddressProvider()))
+            {
+                form.Text = Resources.AddHostEntryDialogTitle;
+
+                // TODO: Fix this default value
+                form.HostEntry = new HostEntry("", "127.0.0.1", null);
+
+                DialogResult result = view.ShowDialog(form);
+
+                if (result == DialogResult.OK)
+                {
+                    this.hostsFile.AddEntry(form.HostEntry);
+
+                    this.ApplyChanges();
+                }
+            }
+        }
+
+        public void EditSelectedEntry()
+        {
+            using (var form = new EditHostEntryForm(view.ServiceProvider, new DnsAddressProvider()))
+            {
+                form.Text = Resources.EditHostEntryDialogTitle;
+
+                form.HostEntry = this.view.SelectedEntries.First();
+
+                DialogResult result = view.ShowDialog(form);
+
+                if (result == DialogResult.OK)
+                {
+                    this.ApplyChanges();
+                }
+            }
+        }
+
+        private void DeleteSelected()
+        {
+            DialogResult result = MessageBox.Show(view, Resources.DeleteEntriesConfirmation, Resources.DeleteEntriesConfirmationTitle,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                foreach (HostEntry entry in this.view.SelectedEntries.ToList())
+                {
+                    hostsFile.DeleteEntry(entry);
+                }
+
+                this.ApplyChanges();
+            }
+        }
+
+        private void EnableSelectedEntries()
+        {
+            DialogResult result = MessageBox.Show(view, Resources.EnableEntriesConfirmation, Resources.EnableEntriesConfirmationTitle,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                foreach (HostEntry entry in this.view.SelectedEntries)
+                {
+                    entry.Enabled = true;
+                }
+
+                this.ApplyChanges();
+            }
+        }
+
+        private void DisableSelectedEntries()
+        {
+            DialogResult result = MessageBox.Show(view, Resources.DisableEntriesConfirmation, Resources.DisableEntriesConfirmationTitle,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.Yes)
+            {
+                foreach (HostEntry entry in this.view.SelectedEntries)
+                {
+                    entry.Enabled = false;
+                }
+
+                this.ApplyChanges();
+            }
+        }
+
+        private void ApplyChanges()
+        {
+            hostsFile.Save();
+
+            this.UpdateData();
+        }
+
+        private void CancelChanges()
+        {
+            this.UpdateData();
+        }
+
+        public void FilterEntries(string text)
+        {
+            this.filter = text;
+
+            this.DisplayEntries();
+        }
+
+        private void DisplayEntries()
+        {
+            var entries = this.hostsFile.Entries;
+
+            if (!String.IsNullOrEmpty(filter))
+            {
+                entries = entries.Where(c =>
+                    c.Address.IndexOf(filter, StringComparison.InvariantCultureIgnoreCase) != -1 ||
+                    c.Hostname.IndexOf(filter, StringComparison.InvariantCultureIgnoreCase) != -1 ||
+                    (c.Comment ?? "").IndexOf(filter, StringComparison.InvariantCultureIgnoreCase) != -1
+                    );
+            }
+
+            view.SetHostEntries(entries);
+        }
+
+        private class ManageHostsTaskList : TaskList
+        {
+            private const string ActionsCategory = "Actions";
+            private const string EnableCategory = "Enable";
+            private const string ChangesCategory = "Changes";
+
+            private ResourceManager iisUIResources = new ResourceManager("Microsoft.Web.Management.Resources", typeof(ModulePage).Assembly);
+
+            private ManageHostsModulePagePresenter owner;
+
+            private bool appliedChanges = false;
+
+            public ManageHostsTaskList(ManageHostsModulePagePresenter owner)
+            {
+                this.owner = owner;
+            }
+
+            public override ICollection GetTaskItems()
+            {
+                ArrayList list = new ArrayList();
+
+                if (owner.HasChanges && appliedChanges)
+                {
+                    appliedChanges = false;
+                }
+
+                IList<HostEntry> selectedEntries = owner.SelectedEntries.ToList();
+
+                bool hasSelection = (selectedEntries.Count > 0);
+                bool multipleSelected = (selectedEntries.Count > 1);
+
+                list.Add(CreateAddTask());
+
+                if (selectedEntries.Count > 0)
+                {
+                    HostEntry firstSelectedEntry = selectedEntries[0];
+
+                    bool firstEntryEnabled = firstSelectedEntry.Enabled;
+
+                    bool showDisableTask = multipleSelected || firstEntryEnabled;
+                    bool showEnableTask = multipleSelected || !showDisableTask;
+
+                    if (!multipleSelected)
+                    {
+                        list.Add(CreateEditTask());
+                    }
+
+                    list.Add(CreateDeleteTask());
+
+                    if (showEnableTask)
+                    {
+                        list.Add(CreateEnableTask());
+                    }
+
+                    if (showDisableTask)
+                    {
+                        list.Add(CreateDisableTask());
+                    }
+                }
+
+                return list;
+            }
+
+            private MethodTaskItem CreateTaskItem(string methodName, string category)
+            {
+                string resourcePrefix = methodName;
+
+                Image taskImage = GetIisImageResource(resourcePrefix);
+
+                return new MethodTaskItem(
+                    methodName,
+                    iisUIResources.GetString(resourcePrefix + "Task"),
+                    category,
+                    iisUIResources.GetString(resourcePrefix + "Description"),
+                    taskImage
+                    );
+            }
+
+            private Image GetIisImageResource(string key)
+            {
+                return (Image)iisUIResources.GetObject(key);
+            }
+
+            public void AddTask()
+            {
+                this.owner.AddTask();
+            }
+
+            public void EditTask()
+            {
+                this.owner.EditSelectedEntry();
+            }
+
+            public void DeleteSelected()
+            {
+                this.owner.DeleteSelected();
+            }
+
+            public void EnableSelected()
+            {
+                this.owner.EnableSelectedEntries();
+            }
+
+            public void DisableSelected()
+            {
+                this.owner.DisableSelectedEntries();
+            }
+
+            #region Task Definitions
+
+            private TaskItem CreateAddTask()
+            {
+                TaskItem addTask = new MethodTaskItem(
+                                "AddTask",
+                                 Resources.AddHostEntryTask,
+                                 ActionsCategory,
+                                 Resources.AddHostEntryDescription
+                                 );
+                return addTask;
+            }
+
+            private TaskItem CreateEditTask()
+            {
+                TaskItem editTask = new MethodTaskItem(
+                                "EditTask",
+                                 Resources.EditHostEntryTask,
+                                 ActionsCategory,
+                                 Resources.EditHostEntryDescription
+                                 );
+                return editTask;
+            }
+
+            private TaskItem CreateDeleteTask()
+            {
+                TaskItem deleteTask = new MethodTaskItem(
+                                "DeleteSelected",
+                                 Resources.DeleteHostEntryTask,
+                                 ActionsCategory,
+                                 Resources.DeleteHostEntryDescription,
+                                 GetIisImageResource("DeleteImage")
+                                 );
+                return deleteTask;
+            }
+
+            private TaskItem CreateEnableTask()
+            {
+                TaskItem enableTask = new MethodTaskItem(
+                                "EnableSelected",
+                                 Resources.EnableHostEntriesTask,
+                                 EnableCategory,
+                                 Resources.EnableHostEntriesDescription
+                                 );
+
+                return enableTask;
+            }
+
+            private TaskItem CreateDisableTask()
+            {
+                TaskItem enableTask = new MethodTaskItem(
+                                "DisableSelected",
+                                 Resources.DisableHostEntriesTask,
+                                 EnableCategory,
+                                 Resources.DisableHostEntriesDescription
+                                 );
+                return enableTask;
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ applications/Setup/source/RichardSzalay.HostsFileExtension.Setup/IisInstallUtil.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RichardSzalay.HostsFileExtension.Setup
+{
+    public static class IisInstallUtil
+    {
+        /// <summary> 
+        /// Registers a new Module in the Modules section inside ApplicationHost.config 
+        /// </summary> 
+        public static void AddModule(string name, string type)
+        {
+            using (ServerManager mgr = new ServerManager())
+            {
+                Configuration appHostConfig = mgr.GetApplicationHostConfiguration();
+                ConfigurationSection modulesSection = appHostConfig.GetSection("system.webServer/modules");
+                ConfigurationElementCollection modules = modulesSection.GetCollection();
+
+                if (FindByAttribute(modules, "name", name) == null)
+                {
+                    ConfigurationElement module = modules.CreateElement();
+                    module.SetAttributeValue("name", name);
+                    if (!String.IsNullOrEmpty(type))
+                    {
+                        module.SetAttributeValue("type", type);
+                    }
+
+                    modules.Add(module);
+                }
+
+                mgr.CommitChanges();
+            }
+        }
+
+        public static void AddUIModuleProvider(string name, string type)
+        {
+            using (ServerManager mgr = new ServerManager())
+            {
+
+                // First register the Module Provider  
+                Configuration adminConfig = mgr.GetAdministrationConfiguration();
+
+                ConfigurationSection moduleProvidersSection = adminConfig.GetSection("moduleProviders");
+                ConfigurationElementCollection moduleProviders = moduleProvidersSection.GetCollection();
+                if (FindByAttribute(moduleProviders, "name", name) == null)
+                {
+                    ConfigurationElement moduleProvider = moduleProviders.CreateElement();
+                    moduleProvider.SetAttributeValue("name", name);
+                    moduleProvider.SetAttributeValue("type", type);
+                    moduleProviders.Add(moduleProvider);
+                }
+
+                // Now register it so that all Sites have access to this module 
+                ConfigurationSection modulesSection = adminConfig.GetSection("modules");
+                ConfigurationElementCollection modules = modulesSection.GetCollection();
+                if (FindByAttribute(modules, "name", name) == null)
+                {
+                    ConfigurationElement module = modules.CreateElement();
+                    module.SetAttributeValue("name", name);
+                    modules.Add(module);
+                }
+
+                mgr.CommitChanges();
+            }
+        }
+
+        /// <summary> 
+        /// Create a new Web Application 
+        /// </summary> 
+        public static void CreateApplication(string siteName, string virtualPath, string physicalPath)
+        {
+            using (ServerManager mgr = new ServerManager())
+            {
+                Site site = mgr.Sites[siteName];
+                if (site != null)
+                {
+                    site.Applications.Add(virtualPath, physicalPath);
+                }
+                mgr.CommitChanges();
+            }
+        }
+
+        /// <summary> 
+        /// Helper method to find an element based on an attribute 
+        /// </summary> 
+        private static ConfigurationElement FindByAttribute(ConfigurationElementCollection collection, string attributeName, string value)
+        {
+            foreach (ConfigurationElement element in collection)
+            {
+                if (String.Equals((string)element.GetAttribute(attributeName).Value, value, StringComparison.OrdinalIgnoreCase))
+                {
+                    return element;
+                }
+            }
+
+            return null;
+        }
+
+        public static void RemoveApplication(string siteName, string virtualPath)
+        {
+            using (ServerManager mgr = new ServerManager())
+            {
+                Site site = mgr.Sites[siteName];
+                if (site != null)
+                {
+                    Application app = site.Applications[virtualPath];
+                    if (app != null)
+                    {
+                        site.Applications.Remove(app);
+                        mgr.CommitChanges();
+                    }
+                }
+            }
+        }
+
+        /// <summary> 
+        /// Removes the specified module from the Modules section by name 
+        /// </summary> 
+        public static void RemoveModule(string name)
+        {
+            using (ServerManager mgr = new ServerManager())
+            {
+                Configuration appHostConfig = mgr.GetApplicationHostConfiguration();
+                ConfigurationSection modulesSection = appHostConfig.GetSection("system.webServer/modules");
+                ConfigurationElementCollection modules = modulesSection.GetCollection();
+                ConfigurationElement module = FindByAttribute(modules, "name", name);
+                if (module != null)
+                {
+                    modules.Remove(module);
+                }
+
+                mgr.CommitChanges();
+            }
+        }
+
+
+        /// <summary> 
+        /// Removes the specified UI Module by name 
+        /// </summary> 
+        public static void RemoveUIModuleProvider(string name)
+        {
+            using (ServerManager mgr = new ServerManager())
+            {
+                // First remove it from the sites 
+                Configuration adminConfig = mgr.GetAdministrationConfiguration();
+                ConfigurationSection modulesSection = adminConfig.GetSection("modules");
+                ConfigurationElementCollection modules = modulesSection.GetCollection();
+                ConfigurationElement module = FindByAttribute(modules, "name", name);
+                if (module != null)
+                {
+                    modules.Remove(module);
+                }
+
+                // now remove the ModuleProvider 
+                ConfigurationSection moduleProvidersSection = adminConfig.GetSection("moduleProviders");
+                ConfigurationElementCollection moduleProviders = moduleProvidersSection.GetCollection();
+                ConfigurationElement moduleProvider = FindByAttribute(moduleProviders, "name", name);
+                if (moduleProvider != null)
+                {
+                    moduleProviders.Remove(moduleProvider);
+                }
+
+                mgr.CommitChanges();
+            }
+        }
+    }
+}
