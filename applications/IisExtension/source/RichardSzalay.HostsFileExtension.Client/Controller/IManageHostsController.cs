45f45fe2f43597cf8d752e53e2c9004f94915bb0
richard_szalay_cp <richard@richardszalay.com>
2011-04-09 17:36:42 +0000

    Fixed up other files affected by SVN-ness when moved to the client project

---
+++ applications/IisExtension/source/RichardSzalay.HostsFileExtension.Client/Controller/IManageHostsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.Management.Client;
+using RichardSzalay.HostsFileExtension.Client.Model;
+
+namespace RichardSzalay.HostsFileExtension.Client.Controller
+{
+    public interface IManageHostsController
+    {
+        IEnumerable<HostEntryViewModel> GetHostEntryModels(Connection connection);
+        IEnumerable<SiteBinding> GetSiteBindings(Connection connection);
+    }
+}
---
+++ applications/IisExtension/source/RichardSzalay.HostsFileExtension.Client/Model/HostEntrySelectionOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RichardSzalay.HostsFileExtension.Client.Model
+{
+    public class HostEntrySelectionOptions
+    {
+        public bool CanEdit { get; set; }
+        public HostEntryField EditableFields { get; set; }
+
+        public bool CanSwitchAddress { get; set; }
+        public ICollection<string> AlternateAddresses { get; set; }
+
+        public bool CanEnable { get; set; }
+        public bool CanDisable { get; set; }
+    }
+}
