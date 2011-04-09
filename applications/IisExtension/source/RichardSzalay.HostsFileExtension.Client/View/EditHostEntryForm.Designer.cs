d100b00129faad022adefc545bdb1e9649015ae8
richard_szalay_cp <richard@richardszalay.com>
2011-04-09 17:35:16 +0000

    Fixed location of client-view classes (svn insanity)

---
+++ applications/IisExtension/source/RichardSzalay.HostsFileExtension.Client/View/EditHostEntryForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RichardSzalay.HostsFileExtension.Client.View
+{
+    partial class EditHostEntryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addressComboBox = new System.Windows.Forms.ComboBox();
+            this.enabledCheckBox = new System.Windows.Forms.CheckBox();
+            this.commentTextBox = new System.Windows.Forms.TextBox();
+            this.commentLabel = new System.Windows.Forms.Label();
+            this.hostnameTextBox = new System.Windows.Forms.TextBox();
+            this.hostnameLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.addressComboBox);
+            this.panel1.Controls.Add(this.enabledCheckBox);
+            this.panel1.Controls.Add(this.commentTextBox);
+            this.panel1.Controls.Add(this.commentLabel);
+            this.panel1.Controls.Add(this.hostnameTextBox);
+            this.panel1.Controls.Add(this.hostnameLabel);
+            this.panel1.Controls.Add(this.addressLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 268);
+            this.panel1.TabIndex = 3;
+            // 
+            // addressComboBox
+            // 
+            this.addressComboBox.FormattingEnabled = true;
+            this.addressComboBox.Location = new System.Drawing.Point(110, 12);
+            this.addressComboBox.Name = "addressComboBox";
+            this.addressComboBox.Size = new System.Drawing.Size(284, 21);
+            this.addressComboBox.TabIndex = 1;
+            this.addressComboBox.TextUpdate += new System.EventHandler(this.OnFormFieldChanged);
+            // 
+            // enabledCheckBox
+            // 
+            this.enabledCheckBox.AutoSize = true;
+            this.enabledCheckBox.Location = new System.Drawing.Point(110, 91);
+            this.enabledCheckBox.Name = "enabledCheckBox";
+            this.enabledCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.enabledCheckBox.TabIndex = 6;
+            this.enabledCheckBox.Text = "&Enabled";
+            this.enabledCheckBox.UseVisualStyleBackColor = true;
+            this.enabledCheckBox.CheckedChanged += new System.EventHandler(this.OnFormFieldChanged);
+            // 
+            // commentTextBox
+            // 
+            this.commentTextBox.Location = new System.Drawing.Point(110, 65);
+            this.commentTextBox.Name = "commentTextBox";
+            this.commentTextBox.Size = new System.Drawing.Size(284, 20);
+            this.commentTextBox.TabIndex = 5;
+            this.commentTextBox.TextChanged += new System.EventHandler(this.OnFormFieldChanged);
+            // 
+            // commentLabel
+            // 
+            this.commentLabel.AutoSize = true;
+            this.commentLabel.Location = new System.Drawing.Point(13, 68);
+            this.commentLabel.Name = "commentLabel";
+            this.commentLabel.Size = new System.Drawing.Size(54, 13);
+            this.commentLabel.TabIndex = 4;
+            this.commentLabel.Text = "&Comment:";
+            // 
+            // hostnameTextBox
+            // 
+            this.hostnameTextBox.Location = new System.Drawing.Point(110, 39);
+            this.hostnameTextBox.Name = "hostnameTextBox";
+            this.hostnameTextBox.Size = new System.Drawing.Size(284, 20);
+            this.hostnameTextBox.TabIndex = 3;
+            this.hostnameTextBox.TextChanged += new System.EventHandler(this.OnFormFieldChanged);
+            // 
+            // hostnameLabel
+            // 
+            this.hostnameLabel.AutoSize = true;
+            this.hostnameLabel.Location = new System.Drawing.Point(13, 42);
+            this.hostnameLabel.Name = "hostnameLabel";
+            this.hostnameLabel.Size = new System.Drawing.Size(58, 13);
+            this.hostnameLabel.TabIndex = 2;
+            this.hostnameLabel.Text = "&Hostname:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(13, 16);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(48, 13);
+            this.addressLabel.TabIndex = 0;
+            this.addressLabel.Text = "&Address:";
+            // 
+            // EditHostEntryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 310);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "EditHostEntryForm";
+            this.Text = "EditHostEntryForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.TextBox hostnameTextBox;
+        private System.Windows.Forms.Label hostnameLabel;
+        private System.Windows.Forms.CheckBox enabledCheckBox;
+        private System.Windows.Forms.TextBox commentTextBox;
+        private System.Windows.Forms.Label commentLabel;
+        private System.Windows.Forms.ComboBox addressComboBox;
+    }
+}
---
+++ applications/IisExtension/source/RichardSzalay.HostsFileExtension.Client/View/EditHostEntryForm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Web.Management.Client.Win32;
+using System.Net;
+using System.Text.RegularExpressions;
+using RichardSzalay.HostsFileExtension.Client.Properties;
+using RichardSzalay.HostsFileExtension.Client.Model;
+
+namespace RichardSzalay.HostsFileExtension.Client.View
+{
+    public partial class EditHostEntryForm : TaskForm
+    {
+        private bool hasChanges = false;
+        private bool canAccept = false;
+
+        private const int MaxTabIndex = 7;
+
+        private string[] addressValues;
+
+        public EditHostEntryForm()
+            : this(null, null)
+        {
+        }
+
+        public EditHostEntryForm(IServiceProvider serviceProvider, IAddressProvider addressProvider)
+            : base(serviceProvider)
+        {
+            InitializeComponent();
+
+            addressLabel.Text = Resources.HostEntryForm_AddressLabel;
+            hostnameLabel.Text = Resources.HostEntryForm_HostnameLabel;
+            commentLabel.Text = Resources.HostEntryForm_CommentLabel;
+            enabledCheckBox.Text = Resources.HostEntryForm_EnabledLabel;
+
+            if (addressProvider != null)
+            {
+                addressValues = addressProvider.GetAddresses();
+                addressComboBox.Items.Clear();
+                addressComboBox.Items.AddRange(addressValues);
+            }
+
+            ((Control)base.AcceptButton).TabIndex = MaxTabIndex + 1;
+            ((Control)base.CancelButton).TabIndex = MaxTabIndex + 2;
+
+            hostnameTextBox.Focus();
+        }
+
+        protected override void OnAccept()
+        {
+            BindFromForm();
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void BindFromForm()
+        {
+            if (addressComboBox.Enabled)
+            {
+                hostEntry.Address = addressComboBox.Text;
+            }
+
+            if (hostnameTextBox.Enabled)
+            {
+                hostEntry.Hostname = hostnameTextBox.Text;
+            }
+
+            if (commentTextBox.Enabled)
+            {
+                hostEntry.Comment = commentTextBox.Text;
+            }
+
+            if (enabledCheckBox.Checked)
+            {
+                hostEntry.Enabled = enabledCheckBox.Checked;
+            }
+        }
+
+        private void BindToForm()
+        {
+            addressComboBox.Text = hostEntry.Address;
+            hostnameTextBox.Text = hostEntry.Hostname;
+            commentTextBox.Text = hostEntry.Comment;
+            enabledCheckBox.Checked = hostEntry.Enabled;
+
+            hasChanges = false;
+        }
+
+        protected override bool CanAccept
+        {
+            get { return canAccept && hasChanges; }
+        }
+
+        private void OnFormFieldChanged(object sender, EventArgs e)
+        {
+            this.hasChanges = true;
+            this.UpdateUIState();
+        }
+
+        private void UpdateUIState()
+        {
+            string address = addressComboBox.Text;
+            string hostname = hostnameTextBox.Text;
+
+            canAccept = (!addressComboBox.Enabled || IsAddressValid(address)) &&
+                (!hostnameTextBox.Enabled || IsHostnameValid(hostname));
+
+            this.UpdateTaskForm();
+        }
+
+        private bool IsHostnameValid(string hostname)
+        {
+            return Regex.IsMatch(hostname, @"^[\w\.\-]+$");
+        }
+
+        private bool IsAddressValid(string address)
+        {
+            return Regex.IsMatch(address, @"^[^\s]+$");
+        }
+
+        private HostEntry hostEntry;
+        private Model.HostEntryField editableFields;
+
+        public HostEntry HostEntry
+        {
+            get { return hostEntry; }
+            set
+            {
+                hostEntry = value;
+
+                this.BindToForm();
+            }
+        }
+
+        public Model.HostEntryField EditableFields
+        {
+            get { return editableFields; }
+            set
+            {
+                editableFields = value;
+
+                addressComboBox.Enabled = ((value & HostEntryField.Address) != 0);
+                hostnameTextBox.Enabled = ((value & HostEntryField.Hostname) != 0);
+                commentTextBox.Enabled = ((value & HostEntryField.Comment) != 0);
+                enabledCheckBox.Enabled = ((value & HostEntryField.Enabled) != 0);
+
+                if (addressComboBox.Enabled)
+                {
+                    addressComboBox.Items.Clear();
+                    addressComboBox.Items.AddRange(addressValues);
+                }
+                else
+                {
+                    addressComboBox.Items.Clear();
+                    addressComboBox.Text = Resources.FieldHasMultipleValues;
+                }
+               
+                if (!hostnameTextBox.Enabled) hostnameTextBox.Text = Resources.FieldHasMultipleValues;
+                if (!commentTextBox.Enabled) commentTextBox.Text = Resources.FieldHasMultipleValues;
+            }
+        }
+    }
+}
