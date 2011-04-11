ac4fcb70ee1841d36f2ac90e89b8b7a2645bf2fe
richard_szalay_cp <SND\richard_szalay_cp>
2011-04-11 19:28:24 +0000

    Added missing files

---
+++ trunk/applications/IisExtension/source/RichardSzalay.HostsFileExtension/Messages/GetServerAddressesRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.Management.Server;
+
+namespace RichardSzalay.HostsFileExtension.Messages
+{
+    public class GetServerAddressesRequest : VoidMessage
+    {
+        public GetServerAddressesRequest()
+        {
+        }
+
+        public GetServerAddressesRequest(PropertyBag bag)
+            : base(bag)
+        {
+        }
+    }
+}
---
+++ trunk/applications/IisExtension/source/RichardSzalay.HostsFileExtension/Service/HostsFileEditCollisionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RichardSzalay.HostsFileExtension.Properties;
+
+namespace RichardSzalay.HostsFileExtension.Service
+{
+    public class HostsFileEditCollisionException : ApplicationException
+    {
+        public HostsFileEditCollisionException()
+            : base(Resources.HostFileEditCollisionException)
+        {
+        }
+    }
+}
